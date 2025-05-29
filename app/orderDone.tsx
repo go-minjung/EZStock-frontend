c08428cb2c396af7c288a34f738359a096b235c9
go-minjung <a01086712346@gmail.com>
2025-05-30 08:23:43 +0900

    feat: 출고, 발주 완료 페이지 구현

---
+++ app/orderDone.tsx
@@ -0,0 +1,49 @@
+import Button from "@/components/Button";
+import { Ionicons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function OrderDoneScreen() {
+  const router = useRouter();
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.content}>
+        <Ionicons name="checkmark-outline" size={64} color="white" />
+        <Text style={styles.text}>발주 완료!</Text>
+      </View>
+
+      <View style={styles.buttonWrapper}>
+        <Button
+          title="홈으로 이동"
+          variant="white"
+          onPress={() => router.push("/")}
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#5A99AB",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingTop: 150,
+  },
+  content: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 30,
+    marginTop: 16,
+  },
+  buttonWrapper: {
+    width: "100%",
+    paddingHorizontal: 20,
+  },
+});
