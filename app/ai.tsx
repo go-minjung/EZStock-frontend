4e29d9e8b1cbc0c3281674c17d84417431666a5b
go-minjung <a01086712346@gmail.com>
2025-05-29 15:45:01 +0900

    feat: 전체 페이지 생성

---
+++ app/ai.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function AIScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>AI 매니저 페이지</Text>
+    </View>
+  );
+}
---
+++ app/deliver.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function DeliverScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>출고 관리 페이지</Text>
+    </View>
+  );
+}
